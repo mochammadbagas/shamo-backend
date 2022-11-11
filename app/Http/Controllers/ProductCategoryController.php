7a39ccc06441275250710c59f03a8fa1cdf1007c
Mochammad Bagas <contact.mochammadbagas@gmail.com>
2022-11-12 03:18:04 +0700

    CRUD Kategori Produk - Index

--- app/Http/Controllers/ProductCategoryController.php
+++ app/Http/Controllers/ProductCategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ProductCategoryRequest;
 use App\Models\ProductCategory;
 use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
 
 class ProductCategoryController extends Controller
 {
@@ -15,7 +16,18 @@ class ProductCategoryController extends Controller
      */
     public function index()
     {
-        //
+        if(request()->ajax()) {
+            $query = ProductCategory::query();
+
+            return DataTables::of($query)
+            ->addColumn('action', function ($item){
+                return '<a class="inline-block border border-gray-700 bg-gray-700 text-white rounded-md px-2 py-1 m-1 transition duration-500 ease-in select-none hover:bg-gray-800 focus:shadow-outline" href="'. route('dashboard.category.edit', $item->id) .'">Edit</a>';
+            })
+            ->rawColumns(['action'])
+            ->make();
+        }
+
+        return view('pages.dashboard.category.index');
     }
 
     /**
